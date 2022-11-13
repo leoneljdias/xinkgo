8f4cd49c5cf848d48b48c3bab61bd27e1c17058c
Leonel Dias <leoneldias@triedeti.pt>
2022-11-13 18:25:07 +0000

    remove console to build

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import vuetify from './plugins/vuetify'
 import firebase from 'firebase/app'
 
 import '@/styles/styles.scss'
-console.log(process.env.VUE_APP_FIREBASE_API_KEY)
+
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
