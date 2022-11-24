4520dad10e0ab2a2337fb0392a4a9a02df432604
Leonel Dias <leoneldias@triedeti.pt>
2022-11-24 23:38:26 +0000

    init user profile page

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,8 +1,32 @@
 <template>
-  <v-btn @click="logOut">Logout</v-btn>
+  <v-container class="fill-height pa-10" v-if="user">
+    <div class="justify-center">
+
+      <h3>Hello, {{ user.displayName }}!</h3>
+
+      <v-avatar class="mt-3" tile style="width:100%; height: 100%; max-width: 400px; border-radius: 5px;">
+        <img :src="user.photoURL" style="width:100%; border: 1px solid black; border-radius: 5px;">
+      </v-avatar>
+
+      <p>
+        <v-btn class="mt-10" width="100%" max-width="400" prepend-icon="mdi-logout" variant="outlined" @click="logOut">
+          Logout
+        </v-btn>
+      </p>
+
+      <p>
+        <v-btn class="mt-3" color="red" width="100%" max-width="400" prepend-icon="mdi-delete-outline"
+          variant="outlined" @click="logOut">
+          DELETE ACCOUNT
+        </v-btn>
+      </p>
+
+    </div>
+  </v-container>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 
@@ -14,7 +38,12 @@ const logOut = async () => {
   await store.dispatch('user/logOut')
   router.push('/')
 }
+
+const user = computed(() => store.state.user.data)
+
+
 </script>
 
 <style>
+
 </style>
